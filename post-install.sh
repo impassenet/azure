61a239562187d692b2cac142aeb79ad96abf8f69
impassenet <42180286+impassenet@users.noreply.github.com>
2021-05-17 16:29:33 +0200

    Add files via upload

---
+++ post-install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#-----------------------------------------------------------------------------------------------------------------
+# Description: Script post installation OVH
+# Createur: Clegrand@integra.fr
+# Version: 0.1
+# OS support: Centos 7.6 (Et superieur) - Centos 8.X - Redhat 7.6 (Et superieur)  - Redhat 8.X - Debian 10.X - Ubuntu 18.X
+#-----------------------------------------------------------------------------------------------------------------
+
+# Update
+yum update -y
+
+# Creation partition LVM production
+echo "n
+p
+1
+
+
+w" | fdisk "/dev/sdb"
+pvcreate -f "/dev/sdb1"
+vgcreate VolGroup01 "/dev/sdb1"
+lvcreate -l +100%FREE -n PRODUCTION VolGroup01
+mkfs.xfs /dev/mapper/VolGroup01-PRODUCTION
+echo '/dev/mapper/VolGroup01-PRODUCTION  /production xfs defaults,noatime 0 2' >> /etc/fstab
+mount /production
